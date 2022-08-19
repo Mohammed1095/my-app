712846fff07d21adc1f11557bd7ca67aba8621da
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2022-08-19 16:58:09 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Movies from "./Components/Movies";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 
+// ///////////////////
+const URL = "https://api.themoviedb.org/3";
+const API_KEY = "530bea0db280cfab0c87d9b46efe504f";
+
+const endpoints = {
+  originals: "/discover/tv",
+  trending: "/trending/all/week",
+};
+// /////////////////////
 function App() {
+  const [originals, setOriginals] = useState([]);
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    // Load Originals
+    axios
+      .get(`${URL}${endpoints.originals}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setOriginals(res.data.results));
+
+    // Load Trending
+    axios
+      .get(`${URL}${endpoints.trending}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setTrending(res.data.results));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero movie={originals[Math.floor(Math.random() * originals.length)]} />
+      <Movies title="Original Movies" movies={originals} />
+      <Movies title="Trending Movies" movies={trending} />
+    </>
   );
 }
 
